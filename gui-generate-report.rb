6060798ea403ddcbdc5b1e2b704e33b54870e94b
Jack Wright <jackw@ooyala.com>
2015-02-19 14:41:09 +0000

    Increased height of app window.

--- gui-generate-report.rb
+++ gui-generate-report.rb
@@ -7,7 +7,7 @@ require './lib/analyticstojson'
 require 'date'
 require './lib/analyticsjsontocsv'
 
-Shoes.app title: "Ooyala Analytics Report Generator", width: 400, height: 200, resizable: false do
+Shoes.app title: "Ooyala Analytics Report Generator", width: 400, height: 260, resizable: false do
   current_date = DateTime.now
   @root_stack = stack margin:0.05 do
     para "Start Date"
