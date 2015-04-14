2f62cb89dc8561d15206a9e2b4299470a6c56642
Jack Wright <jackw@ooyala.com>
2015-04-14 12:19:46 +0100

    Added skeletal version of reportgenerator library.

---
+++ lib/reportgenerator.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+require './lib/analyticstojson'
+require './lib/analyticsv3tojson'
+require 'date'
+require './lib/analyticsjsontocsv'
+require './lib/analyticsv3jsontocsv'
+
+class ReportGenerator
+
+end
