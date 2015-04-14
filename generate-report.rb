06977b72ea1f229091864d36b505ce03b585dc0b
Jack Wright <jackw@ooyala.com>
2015-04-14 12:10:35 +0100

    Placed the code not encapsulated in methods below the method definitions.

--- generate-report.rb
+++ generate-report.rb
@@ -6,44 +6,6 @@ require 'date'
 require './lib/analyticsjsontocsv'
 require './lib/analyticsv3jsontocsv'
 
-#Acquire command line optionsoptions[:secret]
-options = {}
-begin
-  OptionParser.new do |opts|
-
-    opts.banner = "Usage: generate-report.rb -s <start_date> -e <end_date> -o"
-
-    opts.on("-s from_date", "Start date in ISO, e.g. 2014-01-31", :REQUIRED) do |s|
-      options[:from_date] = s
-    end
-
-    opts.on("-e to_date", "End date in ISO, e.g. 2014-01-31", :REQUIRED) do |e|
-      options[:to_date] = e
-    end
-
-    opts.on("-o", "--old", "Use v2 analytics") do
-      options[:v2_analytics] = true
-    end
-  end.parse!
-rescue OptionParser::InvalidOption => s
-  puts s
-  exit(1)
-rescue OptionParser::MissingArgument => e
-  puts e
-  exit(1)
-end
-if !options.has_key?(:from_date)
-  puts "From Date is required"
-  exit(1)
-elsif !options.has_key?(:to_date)
-  puts "To Date is required"
-  exit(1)
-end
-
-if !options.has_key?(:v2_analytics)
-  options[:v2_analytics] = false
-end
-
 # Hacky way of handling custom config. Mainly done for repository management purposes to reduce likelihood of API credentials being committed.
 # System will use config.local.yaml (which isn't checked in) preferentially to config.yaml (which is).
 def getConfig()
@@ -89,6 +51,44 @@ def runReport(start_date_string, end_date_string, v2_analytics)
   puts "Parsing to CSV"
   csvOut.csvFromFile(jsonFilename,csvFilename,daysDifference.to_i+1)
   puts "Done!"
+end
+
+#Acquire command line options
+options = {}
+begin
+  OptionParser.new do |opts|
+
+    opts.banner = "Usage: generate-report.rb -s <start_date> -e <end_date> -o"
+
+    opts.on("-s from_date", "Start date in ISO, e.g. 2014-01-31", :REQUIRED) do |s|
+      options[:from_date] = s
+    end
+
+    opts.on("-e to_date", "End date in ISO, e.g. 2014-01-31", :REQUIRED) do |e|
+      options[:to_date] = e
+    end
+
+    opts.on("-o", "--old", "Use v2 analytics") do
+      options[:v2_analytics] = true
+    end
+  end.parse!
+rescue OptionParser::InvalidOption => s
+  puts s
+  exit(1)
+rescue OptionParser::MissingArgument => e
+  puts e
+  exit(1)
+end
+if !options.has_key?(:from_date)
+  puts "From Date is required"
+  exit(1)
+elsif !options.has_key?(:to_date)
+  puts "To Date is required"
+  exit(1)
+end
+
+if !options.has_key?(:v2_analytics)
+  options[:v2_analytics] = false
 end
 
 runReport(options[:from_date],options[:to_date],options[:v2_analytics])
